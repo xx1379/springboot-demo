daaa29083faf03ab26850b9b94a6e0e1a3946c77
xiaoke <xiaokexkxk@126.com>
2021-10-30 13:13:18 +0800

    init springboot-demo

---
+++ demo-banner/src/main/java/com/xk/banner/DemoBannerApplication.java
@@ -0,0 +1,15 @@
+package com.xk.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Ke Xiao <xiaokexkxk@126.com>
+ * @Date 2021/10/30 12:55
+ */
+@SpringBootApplication
+public class DemoBannerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBannerApplication.class, args);
+    }
+}
